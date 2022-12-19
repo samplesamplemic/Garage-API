7bb8fcdafd57c74ce9717ac436401f9254744761
MLATORRE <michelelt660@gmail.com>
2022-12-19 12:59:40 +0100

    studying

--- src/main/java/com/mic/garage/bootloader/LoadDatabase.java
+++ src/main/java/com/mic/garage/bootloader/LoadDatabase.java
@@ -24,6 +24,8 @@ public class LoadDatabase {
     @Bean
     CommandLineRunner initDatabase(MotoRepository motoRepository, CarRepository carRepository, VanRepository vanRepository) {
         return args -> {
+            //Liskov substitution
+            Vehicle v = new Moto("Honda", 2010, 125, Times.createTimes(2));
 
             carRepository.save(new Car(Doors.createDoors(3), Fuel.DIESEL, "Alfa Romeo", 2011, 1300));
             motoRepository.save(new Moto("Kawasaki", 2013, 30, Times.createTimes(4)));
